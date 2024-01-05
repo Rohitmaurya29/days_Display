9c93b56ae5bab3cff5b6e1d88a6cc4c60b22717e
Rohitmaurya29 <rohith2919maurya@gmail.com>
2024-01-05 13:51:38 +0530

    To_Do_List

--- app.js
+++ app.js
@@ -2,16 +2,34 @@ const express = require('express');
 
 
 const app = express();
+const body= require("body-parser");//to get the html file data
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.set("view engine", "ejs");
+
+
+const item = [""];
 app.get("/",(req,res)=>{
-    // res.send("Started");
-    const weekday = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]
-    const date= new Date();
-    const d= date.getDay();
-    const week = weekday[d];
-   res.send(`${d}: ${week}`);
-   console.log(weekday[new Date().getDay()]);
+    // const name= req.body.name;
+    // const nme= req.body.name;
+    res.render("app", {data:item});
+    // res.send(nme);
 });
+
+app.post("/",(req,res)=>{
+    const name= req.body.name;
+    // res.render("app", {data:name});
+    item.push(name);
+    console.log(name);
+    res.redirect("/");
+});
+
+
+
+
+// app.use(express.bodyParser());
 
 app.listen(8000, ()=>{
     console.log("Hello World..");
